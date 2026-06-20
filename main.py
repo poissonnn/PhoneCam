36da72aa5890cfec54b062731acaac7bc50b3832
poissonnn <xxmoichehxx@gmail.com>
2026-06-21 00:05:30 +0200

    video succesfuly send

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello world")
+#3.12.10
+import cv2
+
+print("cv2")
+
+url = "http://192.168.1.30:4747/video"
+camera = cv2.VideoCapture(url)
+
+
+width = 1600
+height = 1000
+
+cv2.namedWindow("oui")
+cv2.resizeWindow("oui", width, height) 
+
+while True:
+    ret, frame = camera.read()
+
+    frame = cv2.resize(frame, (width,height))
+
+    cv2.imshow("oui",frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
